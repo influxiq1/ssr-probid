b02d3502ad428f0221dd5b788103725a0dc2a5ba
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-01-07 13:11:10 +0530

    koushik home admin rsvp css modification

--- src/app/component/frontend/blog/blog.component.ts
+++ src/app/component/frontend/blog/blog.component.ts
@@ -8,19 +8,7 @@ import { ApiService } from '../../../api.service';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-  public savedUrl: any = [];
-  public savedId: any = [];
-  public blogList: any;
-  public indexval:any = 3;
 
-
-  public ConfigData: any = [
-    { type: 'facebook', link: 'https://SoureshBanerjee.com' },
-    { type: 'twitter', link: 'https://google.com' },
-    { type: 'linkedin', link: 'https://jasonwatmore.com' },
-    { type: 'tumbler', link:'material.angular.io' }
-  ];
-public tokenVal: any;
 
   constructor(public router:Router, public apiService: ApiService) {
 
@@ -28,32 +16,9 @@ public tokenVal: any;
   }
 
 
-
-  blogdetail(val:any){
-    // console.log(val)
-    this.router.navigateByUrl('/blogdetail/' +val)
-  }
-  
-  showMoreFunc(){
-    this.indexval = this.indexval + 3;   
-    // console.log(this.indexval);
-  }
-
   ngOnInit() {
 
-      // console.log(this.tokenVal.length)
-
-    var data: any = {};
-    
-    data = {
-      source:"blogs_view"
-    }
-    this.apiService.getDatalistWithToken(data, "getblogdata").subscribe((res:any)=>{
-
-      this.blogList = res.items;
-      // console.log(this.blogList)
-
-    });
+ 
   }
 
 
