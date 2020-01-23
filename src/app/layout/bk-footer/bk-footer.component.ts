6add7640347f2a11edaa12ea2c6995b2473bbab0
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-01-23 10:05:40 +0530

    PM tool updates

--- src/app/layout/bk-footer/bk-footer.component.ts
+++ src/app/layout/bk-footer/bk-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -12,6 +12,7 @@ export class BkFooterComponent implements OnInit {
   public user_data: any;
   public userCookies: any;
   public userid: any = '';
+  windowScrolled: boolean;
   
   constructor(public cookieService: CookieService, public router: Router, public activeroute: ActivatedRoute ) {
     this.user_data = JSON.parse(this.cookieService.get('user_details'))
@@ -21,6 +22,30 @@ export class BkFooterComponent implements OnInit {
       this.userCookies = JSON.parse(this.cookieService.get('user_details'));
     }
     
+   }
+
+   @HostListener("window:scroll", [])
+
+   onWindowScroll() {
+       if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+           this.windowScrolled = true;
+       }
+       else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+           this.windowScrolled = false;
+       }
+   }
+   
+   scrollToTop() {
+       (function smoothscroll() {
+   
+           var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+   
+           if (currentScroll > 0) {
+               window.requestAnimationFrame(smoothscroll);
+               window.scrollTo(0, currentScroll - (currentScroll / 8));
+           }
+   
+       })();
    }
 
   ngOnInit() {
