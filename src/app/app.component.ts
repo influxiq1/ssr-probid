69ef198d5588d994a9b456c10a59fd254b093372
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-02-04 12:20:28 +0530

    koushik first pull commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-// import {
-//   Event,
-//   NavigationCancel,
-//   NavigationEnd,
-//   NavigationError,
-//   NavigationStart
-// } from '@angular/router';
 
 
 @Component({
@@ -15,29 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // public loading: boolean = false;
   title = 'probid-angular';
   public loader = 0;
+  public loading = 0;
   constructor(public router: Router){
-    /* Universal Loader for Reslove */
-//   this.router.events.subscribe((event: Event) => {
-//     switch (true) {
-//       case event instanceof NavigationStart: {
-//         this.loading = true;
-//         break;
-//       }
-// ​
-//       case event instanceof NavigationEnd:
-//       case event instanceof NavigationCancel:
-//       case event instanceof NavigationError: {
-//         this.loading = false;
-//         break;
-//       }
-//       default: {
-//         break;
-//       }
-//     }
-//   });
+    
 
    }
 }
