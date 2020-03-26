d5efac2aecea0c79b4cbc03a76ec764698017fa2
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-03-26 15:14:51 +0530

    latest css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -447,6 +447,16 @@ const routes: Routes = [
     data: { requestcondition: { source: 'testimonial_view', condition: {} }, endpoint: 'datalist' }, canActivate: [AuthGuard]
   },
 
+  {
+    path: 'testimonial-lists-salesrep', component: ListingTestimonialComponent, resolve: { testimonialList: ResolveService },
+    data: { requestcondition: { source: 'testimonial_view', condition: {} }, endpoint: 'datalist' }, canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'testimonial-lists-customer', component: ListingTestimonialComponent, resolve: { testimonialList: ResolveService },
+    data: { requestcondition: { source: 'testimonial_view', condition: {} }, endpoint: 'datalist' }, canActivate: [AuthGuard]
+  },
+
   // {
   //   path: 'service-listing', component: ListingServiceComponent, resolve: { serviceList: ResolveService },
   //   data: { requestcondition: { source: 'services_view', condition: {} }, endpoint: 'datalist' },canActivate: [AuthGuard]
