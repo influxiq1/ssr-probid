bb9eb7058d4c8a82fbdaef464aed2f1adc2b4fb2
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-03-27 14:16:05 +0530

    breadcamb for all dashboard pages 20200327

--- src/app/component/backend/manage-website/newsletterlists/add-edit-newsletter/add-edit-newsletter.component.ts
+++ src/app/component/backend/manage-website/newsletterlists/add-edit-newsletter/add-edit-newsletter.component.ts
@@ -14,6 +14,7 @@ import { MetaService } from '@ngx-meta/core';
 export class AddEditNewsletterComponent implements OnInit {
 
   public user_details: any;
+  public header_text:any="Add Newsletter"
 
   public configAddEdit: any = {
     action: "add",
@@ -61,6 +62,7 @@ export class AddEditNewsletterComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       if(params._id) {
+        this.header_text="Edit Newsletter"
         this.activatedRoute.data.subscribe(resolveData => {
           this.configAddEdit.defaultData = resolveData.newsData.res[0];
 
