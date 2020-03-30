0db9aa358ca03fe830c692dcc58d160e86a3e9b6
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-03-30 13:57:29 +0530

    newletter css

--- src/app/component/backend/manage-website/newsletterlists/add-edit-newsletter/add-edit-newsletter.component.ts
+++ src/app/component/backend/manage-website/newsletterlists/add-edit-newsletter/add-edit-newsletter.component.ts
@@ -31,8 +31,9 @@ export class AddEditNewsletterComponent implements OnInit {
     userData: { id: "18801017007", name: "Admin" },
     defaultDataAlways: null,
     group_table:'news_category',
-    sender_table:'senders'
-
+    sender_table:'senders',
+    test_mail_table:'testemail',
+    source_for_test_mail_add:'newsTitle_testMail_data'
   
     
   }  
